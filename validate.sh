3c8f93f13a660341de63eb93f77f82995a6a4c76
Brian Eilber <brian.eilber@gmail.com>
2016-02-11 13:48:13 -0500

    Adding variable validation and better logic

--- validate.sh
+++ validate.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-/root/steamcmd/steamcmd.sh +login anonymous +force_install_dir /root/steamcmd/appdir/gmod +app_update 4020 validate +quit
+echo "Begin content validation"
+HOME="/root/steamcmd"
+APPDIR="/root/steamcmd/appdir"
+# A convenience function, to save us some work
+update_server() {
+	# Read the app id and the directory into a variable
 
-/root/steamcmd/steamcmd.sh +login anonymous +force_install_dir /root/steamcmd/appdir/css +app_update 232330 validate +quit
+	APP_ID=$1
+	DIR=$2
 
-/root/steamcmd/steamcmd.sh +login anonymous +force_install_dir /root/steamcmd/appdir/tf2 +app_update 232250 validate +quit
+	# Call SteamCMD with the app ID we provided and tell it to install
+	$HOME/steamcmd.sh +login anonymous +force_install_dir "$APPDIR/$DIR" +app_update $APP_ID validate +quit
+}
 
-cp /mount.cfg /root/steamcmd/appdir/gmod/garrysmod/cfg/
+# Now the script actually runs update_server ( which we just declared above ) with the id of the application ( 4020 is Garry's Mod ) and the name of the directory we want the server to be hosted from:
+
+update_server 4020 "gmod"
+update_server 232330 "css"
+#update_server 232250 "tf2"
+
+# Add any additional servers here by repeating the above, but using a different directory name.
+
+#copy needed files in place
+cp "$APPDIR/css/bin/libstdc++.so.6" "$APPDIR/gmod/bin/"
+cp /mount.cfg "$APPDIR/gmod/garrysmod/cfg/"
+
+# Exit with status code 0 ( which means OK )
+exit 0
+
--- wrapper.sh
+++ wrapper.sh
@@ -1,9 +1,49 @@
 #!/bin/bash
 
+HOME="/root/steamcmd"
+APPDIR="/root/steamcmd/appdir"
+RUNCMD="$APPDIR/gmod/srcds_run -console -game garrysmod"
 
 # if mount cfg not there
-if [ ! -f /root/steamcmd/appdir/gmod/garrysmod/cfg/mount.cfg ]; then
+if [ ! -f "$APPDIR/gmod/garrysmod/bin/libstdc++.so.6" ]; then
+#if [ ! -f /root/steamcmd/appdir/gmod/garrysmod/cfg/mount.cfg ]; then
+	echo "Validtion needed"
+
 	sh /validate.sh
 fi
 
-/root/steamcmd/appdir/gmod/srcds_run -game garrysmod +maxplayers 32 +map gm_construc
+if [ -z "$MAXPLAYERS" ]
+then
+	MAXPLAYERS=20
+fi
+
+RUNCMD="$RUNCMD +maxplayers \"$MAXPLAYERS\""
+
+if [ -z "$MAP" ]
+then
+	MAP="gm_construct"
+fi
+
+RUNCMD="$RUNCMD +map \"$MAP\""
+
+if [ -z "$GAMEMODE" ]
+then
+	GAMEMODE=sandbox
+fi
+
+RUNCMD="$RUNCMD +gamemode \"$GAMEMODE\""
+
+if [ ! -z "$COLLECTON" ]
+then
+	if [ -z "$AUTHKEY" ]
+	then
+		echo "Error, cannot have a collection ID without Authkey"
+		exit 0
+	else
+		RUNCMD="$RUNCMD -authkey \"$AUTHKEY\" +host_workshop_collection \"$COLLECTON\""
+	fi
+fi
+
+echo $RUNCMD
+
+
