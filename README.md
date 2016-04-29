8a01b2273182a3939c05bbd5261c44590011a912
Brian Eilber <brian.eilber@gmail.com>
2016-04-28 20:38:56 -0400

    Accounted for Variables

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # docker-rust
+
+ENV List:
+
+UPDATE - if set to true or TRUE it will validate the cache on startup (RECOMENDED)
+MAXPLAYERS - Set the max number of players for the server (DEF 20)
+HOSTNAME - Sets what the server will be called in the Rust server listing (DEF "Rust Powered by Docker")
+SEED - The map generation seed. Values range from 0 to 429497295 (DEF RANDOM)
+WORLDSIZE - Sets the size of the generated world. The minimum is 2000 and the max is 8000 (DEF 4000)
+IDENTITY - A variable to define different identities for your server. With this you can have multiple server instances. Do not use spaces or special characters. Big fan of the underscore “_”.
+DESCRIPTION - A brief description of the server. For multiple lines, use \n
+URL - Gives a ”VIEW WEBPAGE” option on the server listing screen with this url
+HEADER - Allows you to provide a url to a custom image for the server listing screen. Use a png or jpg of 512 x 256.
+
+
+
+
--- wrapper.sh
+++ wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 HOME="/root/steamcmd"
-RUNCMD="$HOME/steamcmd.sh "
+RUNCMD="$HOME/steamcmd.sh -server.port 28015"
 
 if [ "$UPDATE" == "true" ] || [ "$UPDATE" == "TRUE" ] ; then
 	echo "Validtion needed"
@@ -12,29 +12,50 @@ if [ -z "$MAXPLAYERS" ]
 then
 	MAXPLAYERS=20
 fi
+RUNCMD="$RUNCMD -server.maxplayers \"$MAXPLAYERS\""
 
-RUNCMD="$RUNCMD +maxplayers \"$MAXPLAYERS\""
-
-if [ -z "$MAP" ]
+if [ -z "$HOSTNAME" ]
 then
-	MAP="gm_construct"
+        HOSTNAME="Rust Powered by Docker"
 fi
+RUNCMD="$RUNCMD -server.hostname \"$HOSTNAME\""
 
-RUNCMD="$RUNCMD +gamemode \"$GAMEMODE\""
-
-if [ ! -z "$COLLECTION" ]
+if [ -z "$SEED" ]
 then
-	if [ -z "$AUTHKEY" ]
-	then
-		echo "Error, cannot have a collection ID without Authkey"
-		exit 0
-	else
-		RUNCMD="$RUNCMD -authkey \"$AUTHKEY\" +host_workshop_collection \"$COLLECTION\""
-	fi
-else
-	echo "Collection not defined"
-
+        SEED=`echo $[ 1 + $[ RANDOM % 9999999 ]]`
 fi
+RUNCMD="$RUNCMD -server.seed \"$SEED\""
+
+if [ -z "$WORLDSIZE" ]
+then
+        WORLDSIZE=4000
+fi
+RUNCMD="$RUNCMD -server.worldsize \"$WORLDSIZE\""
+
+if [ -z "$IDENTITY" ]
+then
+        IDENTITY="Rust_Powered_by_Docker"
+fi
+RUNCMD="$RUNCMD -server.identity \"$IDENTITY\""
+
+if [ -z "$DESCRIPTION" ]
+then
+        DESCRIPTION="Rust Powered by Docker"
+fi
+RUNCMD="$RUNCMD -server.description \"$DESCRIPTION\""
+
+if [ -z "$URL" ]
+then
+        URL="https://www.docker.com/"
+fi
+RUNCMD="$RUNCMD -server.url \"$URL\""
+
+if [ -z "$HEADER" ]
+then
+        HEADER="https://www.docker.com/sites/default/files/island_1.png"
+fi
+RUNCMD="$RUNCMD -server.headerimage \"$HEADER\""
+
 
 $RUNCMD
 
